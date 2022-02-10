506b5fd786853a5360351692b0a24bc2dcdd84e4
pjm <2557272503@qq.com>
2022-02-10 21:55:21 +0800

    @ComponentScan这个注解是可以重复定义的：来指定不同的扫描策略

--- src/main/java/com/learning/pjm/config/MainConfig.java
+++ src/main/java/com/learning/pjm/config/MainConfig.java
@@ -1,17 +1,18 @@
 package com.learning.pjm.config;
 
 import com.learning.pjm.entity.Person;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
 @Configuration
-@ComponentScan(value = "com.learning.pjm",includeFilters = {
-        @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class, Service.class})
-},useDefaultFilters = false)
+
+@ComponentScans(value = {
+        @ComponentScan(value = "com.learning.pjm",includeFilters = {
+                @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = {Controller.class, Service.class})
+        },useDefaultFilters = false),
+        @ComponentScan(value = "com.learning.pjm")
+})
 public class MainConfig {
 
     @Bean(value = "person")
